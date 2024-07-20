fdced3479ba4d96b08397a3bb66643b8abd52d05
jongwon <jongwon@DESKTOP-COV8KMR>
2024-07-20 18:21:46 +0900

    aa

--- library-1/src/main/java/com/kg/library_1/LibraryApplication.java
+++ library-1/src/main/java/com/kg/library_1/LibraryApplication.java
@@ -2,10 +2,11 @@ package com.kg.library_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1000) //spring session 기본 30분이나 수정 함
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 
 public class LibraryApplication {
 
